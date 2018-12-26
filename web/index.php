e7adb909f80c9741215192aac3156da1704f361c
Serhiy Hlushko <s.hlushko@gmail.com>
2018-12-26 18:44:15 +0200

    Changed entry points for Heroku env support

--- web/index.php
+++ web/index.php
@@ -1,8 +1,16 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+$debugDefault = getenv('YII_DEBUG');
+if (false === $debugDefault) {
+    $debugDefault = true;
+}
+$envDefault = getenv('YII_ENV');
+if (false === $envDefault) {
+    $envDefault = 'dev';
+}
+defined('YII_DEBUG') or define('YII_DEBUG', $debugDefault);
+defined('YII_ENV') or define('YII_ENV', $envDefault);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
